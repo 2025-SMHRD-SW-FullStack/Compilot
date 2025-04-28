696464edf4422675bc18e280fdb02b81d68205c8
정다훈 <dahoonjeong3572@gmail.com>
2025-04-28 16:23:20 +0900

    asdasd

---
+++ Ex02_1/src/src/TeamClass.java
@@ -0,0 +1,14 @@
+
+public class TeamClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("대접이냐 뚝배기냐");
+		System.out.println(" 비빔국수 3 통일");
+		System.out.println("집중하세요");
+		System.out.println("아메리카노 절반 먹었음");
+		System.out.println("정수리");
+		System.out.println("공차는 2시부터 이벤트 시작인데 그것도 모르고 가서 괜히 돈만 쓰고 온 윤아연 개추");
+	}
+
+}
